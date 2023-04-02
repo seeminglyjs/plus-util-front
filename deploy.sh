cb909b38c1ff04e7ac22ecde54f74b70c6eb4acd
seeminglyjs <sexyhogoo@gmail.com>
2023-04-02 11:07:19 +0900

    20230402코드배포자동화테스트6차

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 export NODE_HOME=/opt/nodejs/node/bin
 export PATH=$PATH:$NODE_HOME
-export yarn_dir="/opt/nodejs/node-v16.19.1-linux-x64/lib/node_modules/yarn/bin"
+#export yarn_dir="/opt/nodejs/node-v16.19.1-linux-x64/lib/node_modules/yarn/bin"
 export pm2_dir="/opt/nodejs/node-v16.19.1-linux-x64/lib/node_modules/pm2/bin"
 
 ROOT=/root
 PROJECT_FOLDER=plus-util-front
-REPOSITORY=/root/plus-util-front 
 ORIGIN=/home/seeminglyjs/actions-runner-front/_work/plus-util-front/plus-util-front
 
 cd $ROOT
-mkdir test1
-rm -rf  test2
 rm -rf $PROJECT_FOLDER
-mkdir $PROJECT_FOLDER
-cd $REPOSITORY 
+cp -r $ORIGIN $ROOT
+cd $PROJECT_FOLDER
 
-$yarn_dir/yarn install
-
-$pm2_dir/pm2 reload all
+pm2 restart plus-util
