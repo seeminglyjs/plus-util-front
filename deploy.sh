df11500d07ab4645855485e79cbfc9ebe5a1e21d
seeminglyjs <sexyhogoo@gmail.com>
2023-04-02 09:40:35 +0900

    20230402코드배포자동화테스트1차

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 export yarn_dir="/opt/nodejs/node-v16.19.1-linux-x64/lib/node_modules/yarn/bin"
 export pm2_dir="/opt/nodejs/node-v16.19.1-linux-x64/lib/node_modules/pm2/bin"
+ROOT=/root
+PROJECT_FOLDER=plus-util-front
 REPOSITORY=/root/plus-util-front 
 
+cd $ROOT
+rm -rf $PROJECT_FOLDER
+mkdir $PROJECT_FOLDER
 cd $REPOSITORY 
 
 $yarn_dir/yarn install
